a319e96ad5b3eaeae30721a3e7fabf99789eb7f3
AllanMisasa <lesvoyagenostalgie@gmail.com>
2025-09-17 13:11:48 +0200

    Lavet en upser

--- test.cpp
+++ test.cpp
@@ -5,15 +5,11 @@
 
 int main() 
 {
-    //std::string fornavn = "allan";
-    //std::string efternavn = "nielsen";
-    //int alder = 31;
     person mig;
     mig.alder = 31, mig.fornavn = "Allan", mig.efternavn = "Nielsen";
     person per;
     per.alder = 50, per.fornavn = "per", per.efternavn = "Dahlstrøm";
     std::cout << mig.alder << std::endl;
     std::cout << per.alder << std::endl;
-
     return 0;
 }
